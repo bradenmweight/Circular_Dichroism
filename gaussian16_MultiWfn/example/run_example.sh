960673c9bbcd28c1f33b23402e5611ace1f80d7f
bradenmweight <braden.m.weight@gmail.com>
2024-12-09 16:44:06 -0500

    Uncommented example lines

--- gaussian16_MultiWfn/example/run_example.sh
+++ gaussian16_MultiWfn/example/run_example.sh
@@ -5,8 +5,8 @@
 
 ##### Run the TDDFT with additional keywords "IOp(6/8=3) IOp(9/40=5)" #####
 #module load gaussian
-#g16 < geometry.com | tee geometry.out
-#formchk geometry.chk geometry.fchk
+g16 < geometry.com | tee geometry.out
+formchk geometry.chk geometry.fchk
 
 ##### Run Multiwfn to get densities as cube files #####
 ../make_transition_density.slurm      | tee make_transition_density.out
@@ -18,5 +18,6 @@ rm -r cube_files
 mkdir cube_files
 mv *cube cube_files/
 
-# Run post-processing script to generate some plots
+# Run post-processing scripts to generate some plots
+python3 ../make_spectroscopy.py
 python3 ../compute_plots_projections.py
